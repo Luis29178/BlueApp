dca9b64e7852f3eaf3c34f7bd23d747b4947b4b8
luis <LEBazaldua@student.fullsail.edu>
2021-05-23 05:04:33 -0400

    BTConnectionService Run Method

--- app/src/main/java/com/example/theblueapp/BTConnectionService.java
+++ app/src/main/java/com/example/theblueapp/BTConnectionService.java
@@ -2,7 +2,9 @@ package com.example.theblueapp;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
 import android.content.Context;
+import android.util.Log;
 
 import java.io.IOException;
 import java.util.UUID;
@@ -37,6 +39,22 @@ public class BTConnectionService {
             sServerSocket = temp;
 
         }
+        public void run()
+        {
+            BluetoothSocket sSocket = null;
+
+            try {
+                sSocket = sServerSocket.accept();
+                Log.d(TAG,"Connection Made");
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+            if (sSocket != null)
+            {
+                Connected(sSocket,sDevice);
+            }
+        }
+
 
 
     }
