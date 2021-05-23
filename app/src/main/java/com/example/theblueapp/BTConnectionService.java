a21bdd7ee64ee727df5e5f328c1f8b05827d6a42
luis <LEBazaldua@student.fullsail.edu>
2021-05-23 05:55:33 -0400

    BTConnectionService ConnectedThread

--- app/src/main/java/com/example/theblueapp/BTConnectionService.java
+++ app/src/main/java/com/example/theblueapp/BTConnectionService.java
@@ -9,6 +9,8 @@ import android.content.Context;
 import android.util.Log;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.UUID;
 
 public class BTConnectionService {
@@ -21,16 +23,17 @@ public class BTConnectionService {
     private BluetoothDevice sDevice;
     private UUID deviceUUID;
     ProgressDialog sProgressDialog;
+    private ConnectedThread sConnectedThread;
 
     private final BluetoothAdapter sBlueAdapter;
     Context sContext;
 
 
-    public BTConnectionService(Context context) {
+    public BTConnectionService(Context context)
+    {
         sContext = context;
         sBlueAdapter = BluetoothAdapter.getDefaultAdapter();
     }
-
     private class AcceptThread extends Thread
     {
         private final BluetoothServerSocket sServerSocket;
@@ -150,6 +153,71 @@ public class BTConnectionService {
         sProgressDialog = sProgressDialog.show(sContext,"Connecting","Please Wait", true);
         sConnectThread = new ConnectThread(_device,_UUID);
         sConnectThread.start();
+    }
+    private class ConnectedThread extends Thread
+    {
+        private final BluetoothSocket sccSocket;
+        private final InputStream _InStream;
+        private final OutputStream _OutStream;
+
+
+        private ConnectedThread(BluetoothSocket sccSocket)
+        {
+            this.sccSocket = sccSocket;
+            InputStream tmpInStream = null;
+            OutputStream tmpOutStream = null;
+            sProgressDialog.dismiss();
+            try {
+                tmpInStream = sccSocket.getInputStream();
+                tmpOutStream = sccSocket.getOutputStream();
+            } catch (IOException ioException) {
+                Log.d(TAG,"Failed In/outStream");
+            }
+            _InStream = tmpInStream;
+            _OutStream =tmpOutStream;
+
+        }
+        public void run()
+        {
+            byte[] buffer = new byte[1024];
+            int bytes;
+            while(true)
+            {
+                try {
+                    bytes = _InStream.read(buffer);
+
+                } catch (IOException ioException) {
+                Log.d(TAG,"Failed INStream read");
+                break;
+                }
+
+            }
+
+        }
+        public void write(byte[] bytes)
+        {
+            try {
+                _OutStream.write(bytes);
+            } catch (IOException ioException) {
+                Log.d(TAG,"OutStream Failed");
+            }
+        }
+
+
+        public void cancel()
+        {
+            try {
+                sccSocket.close();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+
+        }
+
+
+
+
+
     }
 
 }
