ce8d3185f77ff5a56f12a73a66723da7e22f8230
luis <LEBazaldua@student.fullsail.edu>
2021-05-23 05:29:08 -0400

    BTConnectionService startClient Method

--- app/src/main/java/com/example/theblueapp/BTConnectionService.java
+++ app/src/main/java/com/example/theblueapp/BTConnectionService.java
@@ -146,4 +146,11 @@ public class BTConnectionService {
 
     }
 
+    public void startClient(BluetoothDevice _device, UUID _UUID)
+    {
+        sProgressDialog = sProgressDialog.show(sContext,"Connecting","Please Wait", true);
+        sConnectThread = new ConnectThread(_device,_UUID);
+        sConnectThread.start();
+    }
+
 }
