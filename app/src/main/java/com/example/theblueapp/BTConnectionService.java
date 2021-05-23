e1a805fb4e65f50137625ebd4872a7b7d657edcc
luis <LEBazaldua@student.fullsail.edu>
2021-05-23 05:25:24 -0400

    BTConnectionService start Method

--- app/src/main/java/com/example/theblueapp/BTConnectionService.java
+++ app/src/main/java/com/example/theblueapp/BTConnectionService.java
@@ -1,6 +1,8 @@
 package com.example.theblueapp;
 
+import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
@@ -13,6 +15,12 @@ public class BTConnectionService {
     private static final String TAG ="BTCS";
 
     private static final UUID MY_UUID_INSECURE = UUID.fromString("8ce255c0-200a-11e0-ac64-0800200c9a66");//Universal HC_05 modual UUID
+
+    private AcceptThread sInsicureAcceptThread;
+    private ConnectThread sConnectThread;
+    private BluetoothDevice sDevice;
+    private UUID deviceUUID;
+    ProgressDialog sProgressDialog;
 
     private final BluetoothAdapter sBlueAdapter;
     Context sContext;
@@ -66,5 +74,76 @@ public class BTConnectionService {
 
 
 
+
     }
+    private class ConnectThread extends Thread
+    {
+        private BluetoothSocket scSocket;
+        public ConnectThread(BluetoothDevice device, UUID _UUID)
+        {
+            sDevice = device;
+            deviceUUID = _UUID;
+        }
+        public void run()
+        {
+            BluetoothSocket temp = null;
+            try {
+                temp = sDevice.createRfcommSocketToServiceRecord(deviceUUID);
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+            scSocket=temp;
+
+            sBlueAdapter.cancelDiscovery();
+
+            try {
+
+                scSocket.connect();
+
+            } catch (IOException ioException) {
+
+                try {
+                    scSocket.close();
+
+                } catch (IOException e) {
+
+                    //e.printStackTrace();
+                }
+
+                //ioException.printStackTrace();
+            }
+            connected(scSocket,sDevice);
+        }
+        public void cancel()
+        {
+            try {
+
+                scSocket.close();
+
+            } catch (IOException ioException) {
+                //ioException.printStackTrace();
+            }
+
+        }
+
+
+
+    }
+    public synchronized void start()
+    {
+        if (sConnectThread != null)
+        {
+            sConnectThread.cancel();
+            sConnectThread = null;
+        }
+        if (sInsicureAcceptThread == null)
+        {
+            sInsicureAcceptThread = new AcceptThread();
+            sInsicureAcceptThread.start();
+
+        }
+
+
+    }
+
 }
